d94ee145a1a1e4b3e4ff89b1a304fa0671ecc5ee
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-25 10:34:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,18 +3,27 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-	private static final long serialVersionUID = 1L;
-	
-	private Long id;
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
-	
-	public User() {
-	}
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    
+    public User() {
+    }
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
